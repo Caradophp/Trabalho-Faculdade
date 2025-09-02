cc11c105181407fade58d14341c340dd6805c57e
Caradophp <contatolucianofriebe@gmail.com>
2025-09-02 10:27:41 -0300

    Atividades 5 e 6 concluídas

--- exercicios/Exercicio05.java
+++ exercicios/Exercicio05.java
@@ -1,7 +1,26 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio05 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        double saldoMedio = 0.0, credito = 0.0;
+
+        System.out.println("Digite o seu saldo médio: ");
+        saldoMedio = scanner.nextDouble();
+
+        if (saldoMedio <= 2) {
+            credito = 0;
+        } else if (saldoMedio > 200 && saldoMedio <= 400) {
+            credito = saldoMedio * 0.1;
+        } else {
+            credito = saldoMedio * 0.2;
+        }
+
+        System.out.printf("O saldo médio é de: %.2f\n", saldoMedio);
+        System.out.printf("O crédito é de: %.2f", credito);
+
+        scanner.close();
     }
 }
--- exercicios/Exercicio06.java
+++ exercicios/Exercicio06.java
@@ -1,5 +1,35 @@
 package exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio06 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int codigo = 0, pesoToneladas = 0, pesoQuilos = 0;
+        double precoQuilo = 0.0, precoCarga = 0;
+
+        System.out.println("Digite o código da carga: ");
+        codigo = scanner.nextInt();
+
+        System.out.println("Digite o peso da carga (em toneladas): ");
+        pesoToneladas = scanner.nextInt();
+
+        if (codigo >= 10 && codigo <= 20) {
+            precoQuilo = 100;
+        } else if (codigo >= 21 && codigo <=30) {
+            precoQuilo = 250;
+        } else if (codigo >= 31 && codigo <= 40) {
+            precoQuilo = 350;
+        } else {
+            System.out.println("Código inválido");
+        }
+
+        pesoQuilos = pesoToneladas * 1000;
+        precoCarga = precoQuilo * pesoQuilos;
+
+        System.out.println("O peso em quilos é de " + pesoQuilos + " Kg");
+        System.out.printf("O preço da carga do caminhão é de R$ %.2f", precoCarga);
+
+        scanner.close();
+    }
 }
